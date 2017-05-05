492f946667eae1670544206a040500a28f66de5e
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-05 22:42:36 +0500

    Function practice code and doc

---
+++ Lab Java script/functions/app.js
@@ -0,0 +1,314 @@
+// var now = new Date();
+// var theHr = now.getHours();
+// var theMin = now.getMinutes();
+// alert("time: " + theHr + ":" + theMin);
+// document.write("Current time: " + (theHr + ":" + theMin));
+
+
+
+/*
+ function functionname(arg1, arg2, ..., argn) {
+ function body }
+ */
+
+//
+//         /*
+//            function declaration
+//          */
+//        function tellTime()
+//        {
+//            var current = new Date();
+//            var hours   = current.getHours();
+//            var minutes = current.getMinutes();
+//       //      alert(current)
+//            document.getElementById('demo').innerHTML = "Current time: " + (hours + ":" + minutes + ":" + current.getSeconds())
+//        }
+//        /*
+//            function call
+//         */
+//  tellTime();
+//
+
+// alert("hello")
+
+// var checkSaylani = alert
+// console.log(typeof checkSaylani)
+// checkSaylani("Hello World")
+
+//
+//
+//        //lots of code
+//
+
+// myFunc()
+// function myFunc()
+//  {
+//       console.log(123)
+// }
+
+
+
+// alert();
+// function alert() {
+//       console.log("Hello")
+// }
+
+
+
+
+
+
+
+
+//function with parameters
+
+// function increment(num) {  //parameters
+//       console.log(num)
+//       ++num;
+//       document.getElementById('demo').innerHTML = "Number is " + num
+// }
+//  increment(12);//argument
+// increment(34);//argument
+//  increment(100);//argument
+
+
+
+
+
+
+
+//  function increment(num, num2)
+//  {
+// // document.getElementById('demo').innerHTML = "Num is " ?
+//  }
+//  increment(23);
+
+
+
+
+
+//  function increment(num)
+//  {
+// // // document.getElementById('demo').innerHTML = "Num is " ?
+//  }
+//  increment(4, 7, 9);
+
+
+
+
+
+
+
+// function add(num1, num2) //parameters
+// {
+//       var sum = num1 + num2;
+//       document.getElementById('demo').innerHTML = "Sum is " + sum
+// }
+//
+//  add(4 , 2); //arguments
+//        add(56, 980);
+
+
+
+
+
+
+
+
+// function greetUser(username) {
+//       document.getElementById('demo').innerHTML = "Welcome, " + username
+// }
+// greetUser();
+// greetUser("Sami");
+// greetUser("Sami", "Khan");
+
+
+
+//  greetUser("Ali");
+//  var myName = "Ali";
+//  var myName = prompt('your name)
+//  greetUser(myName);
+
+
+
+// function myFunction(num) {
+//       var sum = num + 10
+//       console.log(sum)
+//       // var institudeName = "Saylani mass"
+//       // alert(institudeName)
+//       return "num + 10 = 15"
+// }
+
+// alert(myFunction(5))
+
+// RETURN
+
+
+
+
+
+
+//        function getGrade(percentage)
+//        {
+//            if(percentage >= 80)
+//            {
+//                alert("A-one grade");
+//            }
+//            else if(percentage >= 70)
+//            {
+//                alert("A grade");
+//            }
+//            else if(percentage >= 60)
+//            {
+//                alert("B grade");
+//            }
+//
+//            return
+//        }
+//
+//
+//        //var percentage = parseInt(prompt("What is your percentage"));
+//        //getGrade(percentage);
+//        getGrade(82);
+
+
+
+
+
+
+
+
+
+//  function getGrade(percentage)
+//  {
+//      var grade;
+//      if(percentage >= 80)
+//      {
+//          grade = "A-one grade";
+//      }
+//      else if(percentage >= 70)
+//      {
+//          grade = "A grade";
+//      }
+//      else if(percentage >= 60)
+//      {
+//          grade = "B grade";
+//      }
+//      return grade;
+//  }
+//        alert(getGrade(89));
+//        document.write(getGrade(89));
+//        console.log(getGrade(89));
+//        var grade = getGrade(89);
+//
+
+
+
+
+
+
+
+
+
+// function convertToUpperCase(str) {
+//       return str.toUpperCase();
+// }
+
+// var uppercase = convertToUpperCase("ali khan");
+// alert(uppercase);
+
+// document.write(convertToUpperCase("Majid khan"));
+
+// console.log(convertToUpperCase("khan"));
+
+
+
+
+
+
+
+
+
+
+// function calcShippingCharges(amount)// 14s
+// {
+//       return amount + 5;
+// }
+// //var res = 10 + calcShippingCharges(4); //19
+// if (calcShippingCharges(9) >= 10) {
+//       alert("Good");
+// }
+
+
+
+
+
+
+
+// var message = "Hi";     //global variable
+// function sayHello()
+// {
+// alert(message);
+// }
+// alert(message);
+// sayHello();
+
+
+
+// var message = "Hi";
+// function sayHello(num)
+// {
+// var message = "Hello";//
+// alert(message);
+// }
+// sayHello();
+// alert(message);
+
+
+// setTimeout(timer, 3000)
+
+
+
+
+
+// function sayHello(num)
+// {
+//     message = "Hello";
+//     //alert(message);
+// }
+
+// sayHello();
+// alert(message);
+
+
+// function timer(arg) {
+//       console.log(arg)
+//       setInterval(function () {
+//             document.getElementById('time').innerHTML = new Date().toLocaleTimeString()
+//       }, arg)
+// }
+
+
+// document.getElementById('demo').innerHTML = "Stop" + new Date().toLocaleTimeString()
+
+
+
+
+
+      //  function add()
+      //  {
+      //      console.log(arguments);
+      //  }
+
+      //  add(); //0
+      //  add(3); //1
+      //  add(5,7); //2
+
+
+
+
+
+
+// function cal(arg) {
+//       console.log(arg)
+// }
