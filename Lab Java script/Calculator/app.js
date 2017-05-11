d110e34ce5b94622d94c175145185aad8a792ab0
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-11 16:26:48 +0500

    Implement Operator function and show on display

--- Lab Java script/Calculator/app.js
+++ Lab Java script/Calculator/app.js
@@ -1,6 +1,16 @@
 var display = document.getElementById('display'),
       value1, value2, operator
 
-function operator() { }
-function cal() { }
+
+// This function work for operators
+function operat(sign) {
+      console.log(sign)
+      display.innerHTML = sign
+}
+
+//This function get number value
+function cal(num) {
+}
+
+// This fucntion work to equal
 function equal() { }
