d63dcb94bdf4fec637d64af15cdbfdeb5c1eff41
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-05-11 16:50:55 +0500

    Implement logic to get every number to calculate

--- Lab Java script/Calculator/app.js
+++ Lab Java script/Calculator/app.js
@@ -5,11 +5,33 @@ var display = document.getElementById('display'),
 // This function work for operators
 function operat(sign) {
       console.log(sign)
+      operator = sign
       display.innerHTML = sign
 }
 
 //This function get number value
 function cal(num) {
+      num = num.toString()
+
+      if (operator == undefined) {
+            if (value1 == undefined) {
+                  value1 = num
+            } else {
+                  value1 += num
+            }
+            display.innerHTML = value1
+      } else {
+            if (value2 == undefined) {
+                  value2 = num
+            } else {
+                  value2 += num
+            }
+            display.innerHTML = value2
+      }
+
+      console.log('value1', value1)
+      console.log('value2', value2)
+      console.log('operator', operator)
 }
 
 // This fucntion work to equal
