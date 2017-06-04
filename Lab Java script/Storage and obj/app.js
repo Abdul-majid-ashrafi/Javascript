a8eb15648e53c0579dfd744263ae63bf6ddd22d3
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2017-06-04 11:06:25 +0500

    update

---
+++ Lab Java script/Storage and obj/app.js
@@ -0,0 +1,83 @@
+// var text = "hello Saylani"
+// function add() {
+//     localStorage.setItem('data', text)
+// }
+
+// function getData() {
+// var getStorage = localStorage.getItem('data')
+// document.getElementById('demo').innerHTML = getStorage
+// }
+
+// function removed() {
+//     localStorage.removeItem('data')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var myObj = {
+//     userName: "Majid",
+//     age: 25,
+//     // num : ['03083232332','03083234342']
+// }
+// function add() {
+//     myObj.email = "majid@gmail.com"
+// console.log(myObj)
+// }
+
+
+
+
+// function add() {
+//     var getInputVal = document.getElementById('demo').value
+//     var obj = {
+//         userName: getInputVal,
+//         age: 12345
+//     }
+//     localStorage.setItem('data', JSON.stringify(obj))
+// }
+
+// function getData() {
+//     var getData = localStorage.getItem('data')
+//     console.log(JSON.parse(getData))
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
